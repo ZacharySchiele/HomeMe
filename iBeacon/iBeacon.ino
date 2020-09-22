2322d12f11c5ecc85d697fc53ded17c100158719
jackanderson12 <43770968+jackanderson12@users.noreply.github.com>
2020-09-22 12:56:55 -0400

    Beacon Code Updated

--- iBeacon/iBeacon.ino
+++ iBeacon/iBeacon.ino
@@ -1,35 +1,59 @@
+/*
+
+*/
 #include <ArduinoBLE.h>
 
-//BLE service to send RSSI value
-BLEService sendRSSI("180FF");
+BLEService IPC_Service("0x2AAD");
+BLEService FN_Service("0x2AB2");
+BLEService LN_Service("0x2AB5");
+BLEService P3D_Service("0x2A30");
+BLEService RN_Service("0x2A3D");
 
-//BLE RSSI Level Characteristic
-BLEUnsignedCharCharacteristic sendRSSIChar("2A19",  // standard 16-bit characteristic UUID
-    BLERead | BLENotify);  //remote clients will be able to get notifications if this characteristic changes
-
-int oldRSSI = 0;  // last rssi reading from analog input 
-long previousMillis = 0;  // last time the rssi was checked
+//Creates a gatt characteristic that holds the Indoor Position Configuration, both readable and writable by central
+BLEUnsignedIntCharacteristic IndoorPositioningConfiguration ("0x2AAD", BLERead | BLEWrite);
+//Creates a gatt characteristic that holds the floor number, both readable and writable by central
+BLEUnsignedIntCharacteristic FloorNumber ("0x2AB2", BLERead | BLEWrite);
+//Creates a gatt characteristic that holds the Location Name, both readable and writable by central
+BLEUnsignedCharCharacteristic LocationName ("0x2AB5", BLERead | BLEWrite);
+//Creates a gatt characteristic that holds the 3D Positioning, both readable and writable by central
+BLEUnsignedIntCharacteristic Position3D ("0x2A30", BLERead | BLEWrite);
+//Creates a gatt characteristic that holds the Routine Name, both readable and writable by central
+BLEUnsignedCharCharacteristic RoutineName ("0x2A3D", BLERead | BLEWrite);
 
 void setup() {
   Serial.begin(9600);    // initialize serial communication
   while (!Serial);
 
   pinMode(LED_BUILTIN, OUTPUT); // initialize the built-in LED pin to indicate when a central is connected
-
-  // begin initialization
+  //intializes the bluetooth ble beacon
   if (!BLE.begin()) {
     Serial.println("starting BLE failed!");
 
     while (1);
   }
   
-  BLE.setDeviceName("iBeacon"); // set the bluetooth to show up as iBeacon
-  BLE.setLocalName("Beacon1"); // set the local name of the iBeacon to Beacon1
-  BLE.setAdvertisedService(sendRSSI); // add the service UUID
-  sendRSSI.addCharacteristic(sendRSSIChar); // add the rssi level characteristic
-  BLE.addService(sendRSSI); // add the sendRSSI service
-  sendRSSIChar.writeValue(oldRSSI); // set the initial rssi for this characteristic
-
+  BLE.setDeviceName("Beacon");
+  
+  BLE.setAdvertisedService(IPC_Service);
+  IPC_Service.addCharacteristic(IndoorPositioningConfiguration); 
+  BLE.addService(IPC_Service);
+  
+  BLE.setAdvertisedService(FN_Service);
+  FN_Service.addCharacteristic(FloorNumber); 
+  BLE.addService(FN_Service);
+  
+  BLE.setAdvertisedService(LN_Service);
+  LN_Service.addCharacteristic(LocationName); 
+  BLE.addService(LN_Service);
+  
+  BLE.setAdvertisedService(P3D_Service);
+  P3D_Service.addCharacteristic(Position3D); 
+  BLE.addService(P3D_Service);
+  
+  BLE.setAdvertisedService(RN_Service);
+  RN_Service.addCharacteristic(RoutineName); 
+  BLE.addService(RN_Service); 
+    
   //Start advertising BLE. It will start continously transmitting BLE
   //advertising packets and will be visible to remote BLE central devices
   //until it receives a new connection
@@ -37,48 +61,45 @@ void setup() {
   // start the advertisement
   BLE.advertise();
   Serial.println("Bluetooth beacon active, waiting for connections...");
-  
 }
 
 void loop() {
-  // wait for a BLE central
+  
+  //initializes central devices to allow for connections to central devices
   BLEDevice central = BLE.central();
-
-  // if a central is connected to the peripheral:
-  if (central) {
+  
+  dummyVals();
+  
+  //Wait for a connection to the centtral then do something
+  if (central){
+   
     Serial.print("Connected to central: ");
     // print the central's BT address:
     Serial.println(central.address());
     // turn on the LED to indicate the connection:
     digitalWrite(LED_BUILTIN, HIGH);
-
-    // check the RSSI every 200ms
-    // while the central is connected:
-    while (central.connected()) {
-      long currentMillis = millis();
-      // if 200ms have passed, check the RSSI:
-      if (currentMillis - previousMillis >= 200) {
-        previousMillis = currentMillis;
-        updateRSSI();
-      }
+    
+    while (central.connected()){
+      
     }
-    // when the central disconnects, turn off the LED:
     digitalWrite(LED_BUILTIN, LOW);
     Serial.print("Disconnected from central: ");
     Serial.println(central.address());
   }
-  loop();
+  
 }
 
-void updateRSSI() {
-  //Read the RSSI value from the central
-  int rssiValue =  BLE.rssi();
-  int rssiLevel = map(rssiValue, -128, 128, -128, 128);
-
-  if(rssiValue != oldRSSI){
-    Serial.print("RSSI value is now: "); // print the rssi value
-    Serial.println(rssiValue); 
-    sendRSSIChar.writeValue(rssiLevel); // update the rssi characteristic
-    oldRSSI = rssiLevel; // save the level for comparison
-  }
+void dummyVals(){
+  int IPC = 0;
+  int FloorNumberInt = 0;
+  char LocationNameString = 'R';
+  int ThreeDPosition = 0;
+  char RoutineNameString = 'R';
+  //Dummy values
+  IndoorPositioningConfiguration.writeValue(IPC);
+  FloorNumber.writeValue(FloorNumberInt);
+  LocationName.writeValue(LocationNameString);
+  Position3D.writeValue(ThreeDPosition);
+  RoutineName.writeValue(RoutineNameString);
+  
 }
